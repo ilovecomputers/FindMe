7add8c42e78dbe6d911c74dda6f9b9bb7dcc10b4
i❤computers <i.love.comput3rs@gmail.com>
2015-02-19 20:05:30 -0800

    Add colors

--- src/app/color/colors.js
+++ src/app/color/colors.js
@@ -14,7 +14,17 @@ angular.module( 'FindMe.Color', [
       templateUrl: 'color/color.tpl.html'
     });
 })
-
+.factory('Colors', function(){
+  return [
+    {color: 'red', hex: randomColor({ luminosity: 'bright', hue: 'red' })},
+    {color: 'orange', hex: randomColor({ luminosity: 'bright', hue: 'orange' })},
+    {color: 'yellow', hex: randomColor({ luminosity: 'bright', hue: 'yellow' })},
+    {color: 'green', hex: randomColor({ luminosity: 'bright', hue: 'green' })},
+    {color: 'blue', hex: randomColor({ luminosity: 'bright', hue: 'blue' })},
+    {color: 'purple', hex: randomColor({ luminosity: 'bright', hue: 'purple' })},
+    {color: 'pink', hex: randomColor({ luminosity: 'bright', hue: 'pink' })}
+  ];
+})
 .controller( 'ColorsCtrl', function ColorsController( $scope, Colors ) {
     $scope.colors = Colors;
 })
